fc244a61ff3aa869463e655922efaa84d19b20f5
janiceparkk <jparkowl@gmail.com>
2024-07-25 12:33:22 -0700

    Basic Game Loop

--- main.cpp
+++ main.cpp
@@ -2,11 +2,21 @@
 #include <raylib.h>
 
 int main () {
-    const::int screen_width = 12800;
-    const::int screen_height = 800;
+    const int screen_width = 1280;
+    const int screen_height = 800;
 
     InitWindow(screen_width, screen_height, "My Pong Game");
+    SetTargetFPS(60); // How fast the game should run (60 frames per sec)
 
+    // Game Loop
+    while (WindowShouldClose() == false) {
+        BeginDrawing();
+        // 1. Check for events
+        // 2. Update positions of game objects
+        // 3. Draw game objects in their new positions
+        EndDrawing();
+    }
+    
     CloseWindow();
     return 0;
 }
