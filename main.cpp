812cbaed8b50b4f2a14e925b267ab4d3a5a00ccf
janiceparkk <jparkowl@gmail.com>
2024-07-25 12:25:08 -0700

    Basic Game Window Set Up

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <raylib.h>
+
+int main () {
+    const::int screen_width = 12800;
+    const::int screen_height = 800;
+
+    InitWindow(screen_width, screen_height, "My Pong Game");
+
+    CloseWindow();
+    return 0;
+}
