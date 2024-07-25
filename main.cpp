91efcb0b07eb7b27b7604fad7f19a38c603fcf0d
janiceparkk <jparkowl@gmail.com>
2024-07-25 13:29:26 -0700

    Animated Ball & Basic Paddle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
 #include <raylib.h>
 
+class Ball {
+    public:
+        float x, y;
+        int speed_x, speed_y;
+        int radius;
+
+        void Draw() {
+            DrawCircle(x, y, radius, WHITE);
+        }
+
+        void Update() {
+            x += speed_x;
+            y += speed_y;
+
+            // don't get out of bounds
+            if (y + radius >= GetScreenHeight() || y - radius <= 0) { 
+                speed_y *= -1;
+            }
+
+            if (x + radius >= GetScreenWidth() || x - radius <= 0) {
+                speed_x *= -1;
+            }
+        }
+};
+
+class Paddle {
+    public:
+        float x, y;
+        float width, height;
+        int speed;
+
+        void Draw() {
+            DrawRectangle(x, y, width, height, WHITE);
+        }
+
+        void Update() {
+            // paddle moves up when up key is pressed
+            if (IsKeyDown(KEY_UP)) {
+                y -= speed;
+            }
+
+            if (IsKeyDown(KEY_DOWN)) {
+                y += speed;
+            }
+
+            // paddle can't go out of bounds
+            if (y <= 0) {
+                y = 0;
+            }
+
+            if (y + height >= GetScreenHeight()) {
+                y = GetScreenHeight() - height;
+            }
+        }
+};
+
+// class CpuPaddle: public Paddle {
+
+// }
+
+Ball ball;
+Paddle player;
+
 int main () {
     const int screen_width = 1280;
     const int screen_height = 800;
@@ -8,16 +71,32 @@ int main () {
     InitWindow(screen_width, screen_height, "My Pong Game");
     SetTargetFPS(60); // How fast the game should run (60 frames per sec)
 
+    ball.radius = 20;
+    ball.x = screen_width/2;
+    ball.y = screen_height/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height/2 - player.height/2;
+    player.speed = 6;
+
     // Game Loop
     while (WindowShouldClose() == false) {
         BeginDrawing();
         // 1. Check for events
+        
         // 2. Update positions of game objects
+        ball.Update();
+        player.Update();
+
         // 3. Draw game objects in their new positions
+        ClearBackground(BLACK); // clear the previous position of white ball
         DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
-        DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
-        DrawRectangle(0, screen_height/2 - 60, 25, 120, WHITE);
-        DrawRectangle(screen_width - 35, screen_height/2 - 60, 25, 120, WHITE);
+        ball.Draw();
+        player.Draw();
         EndDrawing();
     }
     
